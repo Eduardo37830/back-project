26f0f1418fcb1795b4329f3d9d3a785343030c4e
Eduardo Villamil <eduardo.villamil37830@ucaldas.edu.co>
2025-04-02 20:28:01 -0500

    feat: cors

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@ require('dotenv').config();
 const connectionDB = require('./config/database');
 const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 const port =process.env.PORT || 3005;
 
 //console.log(app);
-
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 // listen: 2 param puerto y funcion flecha
 app.listen(port, () => {
     console.log(`Project running on port ${port}`);
 
 });
+
+app.options("*", cors());
 
 app.use(bodyParser.json())
 app.use('/api/v1', routes)
