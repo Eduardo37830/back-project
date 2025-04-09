5409cc18a29573ba780ba4fe03250d340c43bc66
Andres16HKMA <kamzyx2004@gmail.com>
2025-04-09 01:05:34 -0500

    cambio para cors

--- src/index.js
+++ src/index.js
@@ -6,36 +6,21 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
-const port =process.env.PORT || 3005;
+const port = process.env.PORT || 3005;
 
-//console.log(app);
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-// listen: 2 param puerto y funcion flecha
+
+app.use(bodyParser.json());
+app.use('/api/v1', routes);
+
+// app.options("*", cors());  <-- Comenta o elimina esta línea
+
 app.listen(port, () => {
     console.log(`Project running on port ${port}`);
-
 });
-
-app.options("*", cors());
-
-app.use(bodyParser.json())
-app.use('/api/v1', routes)
-
-
-/* Repaso funcion flecha */
-
-// const printMessageArrow = () => {
-//     return `Segun la extrapolacion de texto y variables tenemos: ${port}`;
-// }
-/* Repaso funcion anonima */
-// const printMessage = function() {
-//     return `Segun la extrapolacion de texto y variables tenemos: ${port}`;
-// }
-
-// console.log(printMessage());
 
 connectionDB();
