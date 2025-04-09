f700ae3a7653def9e12f3dc08bc5650627805399
Juan Manuel Ocampo <juan.1701911769@ucaldas.edu.com>
2025-04-09 00:52:44 -0500

    Feat: save deparments and cities from csv file

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const authRoutes = require("./AuthRoutes");
-const userRoutes = require("./UserRoutes");
+const authRoutes = require('./AuthRoutes');
+const userRoutes = require('./UserRoutes');
+const townsRoutes = require('./TownsRoutes');
 
-router.use("/auth", authRoutes);
+router.use('/auth', authRoutes);
 
-router.use("/users", userRoutes);
+router.use('/users', userRoutes);
+
+router.use('/towns', townsRoutes);
 
 module.exports = router;
 
